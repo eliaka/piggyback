586fb57c2a46d95438d2a5637c1aced908570138
eliaka <eliaka@users.noreply.github.com>
2020-12-20 14:17:20 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -244,7 +244,7 @@ class Manager(object):
             errors = self.eval()
             error_history.append(errors)
             accuracy = 100 - errors[0]  # Top-1 accuracy.
-            loss = self.criterion(output, label) ####
+            #### loss = self.criterion(output, label) ####
 
             # Save performance history and stats.
             with open(savename + '.json', 'w') as fout:
